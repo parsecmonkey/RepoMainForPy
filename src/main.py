e71de33454c7064873eebe32372041af18d72f30
parsecmonkey <daisuke-1120@i.softbank.jp>
2021-11-19 02:04:51 +0900

    fix rmtree

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 import git
 import os
 import shutil
+import subprocess
 
 url = 'https://github.com/parsecmonkey/RepoMainForPy'
 
 to_path = 'project' # cloneしたプロジェクトを出力するパス
 delete_path = "project"
 
-# projectフォルダが存在していれば削除
-if(os.path.isdir('project/') == True):
-    shutil.rmtree('project/')
-        
+# フォルダを削除
+def rmtree(top):
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            filename = os.path.join(root, name)
+            os.chmod(filename, 0o777)
+            os.remove(filename)
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(top) 
+
+rmtree(delete_path) # projectフォルダが存在していれば削除
+
 git.Repo.clone_from(url, to_path)
 
 # repo = git.Repo()
